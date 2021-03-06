b3c1a71e1858fa50e7811d5cd59369aa2f52ad81
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2021-03-06 13:36:01 -0800

    Create merge-k-sorted-lists.cpp

---
+++ pqueue/merge-k-sorted-lists.cpp
@@ -0,0 +1,35 @@
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        for(int i = 0; i < lists.size(); ++i) {
+            ListNode* head = lists[i];
+            while(head) {
+                pq.push(head->val);
+                head = head->next;
+            }
+        }
+        
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr = dummy;
+        
+        while(!pq.empty()) {
+            curr->next = new ListNode(pq.top());
+            pq.pop();
+            curr = curr->next;
+        }
+        
+        curr = dummy->next;
+        delete dummy;
+        return curr;
+    }
+};
